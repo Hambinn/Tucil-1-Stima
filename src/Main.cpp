c16634130e12d4805568785a2cf655caf21b23a4
Hambinn <ilhambintangn@gmail.com>
2022-01-26 08:45:16 +0700

    last commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <vector>
 #include <charconv>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 // masukin index nya ke 2 vector berbeda aja terus nanti index yg di print nya dari 2 vector itu
 
@@ -11,8 +13,6 @@ bool checkIndex(int idxi, int idxj, vector<vector<int>> idsi, vector<vector<int>
     for(int i=0; i<idsi.size();i++){
         for(int j=0;j<idsi[i].size();j++){
             if(idsi[i][j]==idxi && idsj[i][j]==idxj){
-                // cout << "soal: "<<idxi << " " << idxj << endl;
-                // cout << "jawaban: "<<idsi[i][j] << " " << idsj[i][j] << " "<< i << " " << j << " " <<endl;
                 return true;
             }
         }
@@ -27,16 +27,15 @@ int main(){
     vector<char> ans;
     vector<string> soal;
     string kosong;
-    string delimiter = " ";
     vector<int> indexi;
     vector<int> indexj;
     vector<vector<int>> indicesi;
     vector<vector<int>> indicesj;
     int check = 0;
+    int huruf = 0;
     string filename;
-    vector<string> color = {{"\033[1;31m"}, {"\033[1;32m"}, {"\033[1;33m"}, {"\033[1;34m"}, {"\033[1;35m"}, {"\033[1;36m"}, {"\033[1;37m"}};
     
-    // take input from user
+    // input dari user
     cout << "Masukkan nama file (spesifik dengan tempatnya): ";
     cin >> filename;
     ifstream myfile(filename);
@@ -62,22 +61,20 @@ int main(){
                 soal[i].erase(j,1);
             }
         }
-        // cout << soal[i] << "\n";
     }
 
     for(int k = 0; k<2;k++){
         getline(myfile, kosong, '\n');
     }
 
-    cout << "\n";
     int i = 0;
     while(getline(myfile, plc, '\n')){
         jawab.push_back(plc);
-        // cout << jawab[i] << "\n";
         i++;
     }
     
-    cout << "\n";
+    auto start = high_resolution_clock::now();
+
     for(int i = 0; i<jawab.size();i++){ // looping jawaban ke bawah
             int count = 0;
             ans.clear();
@@ -95,11 +92,14 @@ int main(){
                     indexj.clear();
                     count = 0;
                     if(jawab[i][j] == soal[k][l]){ // kalo jawaban sama dengan soal 
+                        huruf += 1;
                         if(jawab[i][j+1]== soal[k][l+1] && jawab[i][j+2]== soal[k][l+2] && j!= jawab[i].length()-1){ // kalo jawaban+1 sama dengan soal+1 (arah kanan)
+                                huruf += 1;
                                 for(int m=0;m<jawab[i].length();m++){ // looping jawaban ke samping
                                     for(int n=l;n<soal[0].length();n++){ // looping soal ke samping
                                         n += count;
                                         if(jawab[i][m] == soal[k][n]){
+                                            huruf += 1;
                                             indexi.push_back(k);
                                             indexj.push_back(n);
                                             ans.push_back(soal[k][n]); // disini
@@ -121,10 +121,12 @@ int main(){
                                 }                       
                         } 
                         else if(jawab[i][j+1]== soal[k][l-1] && jawab[i][j+2]== soal[k][l-2] &&  j!= jawab[i].length()-1){ // kalo jawaban+1 sama dengan soal-1 (arah kiri)
+                                huruf += 1;
                                 for(int m=0;m<jawab[i].length();m++){ // looping jawaban ke samping
                                     for(int n=l;n>=0;n--){ // looping soal ke samping
                                         n -= count;
                                         if(jawab[i][m] == soal[k][n]){
+                                            huruf += 1;
                                             indexi.push_back(k);
                                             indexj.push_back(n);
                                             ans.push_back(soal[k][n]);
@@ -148,10 +150,13 @@ int main(){
                                 }
                             }
                         else if(k == soal.size()-2 || k == soal.size()-1){
+                            huruf +=1;
                             if((jawab[i][j+1] == soal[k-1][l]) && jawab[i][j+2] == soal[k-2][l]){
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k-count;n>=0;n--){
                                                 if(jawab[i][m] == soal[n][l] && (n!= 0 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(l);
                                                     ans.push_back(soal[n][l]);
@@ -180,11 +185,13 @@ int main(){
 
                                 }
                                 else if(jawab[i][j+1] == soal[k-1][l-1]){
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k;n>=0;n -= count){ // loop bawah soal
                                                 for(int o=l;o>=0;o--){
                                                     o -= count; // loop kanan soal
                                                     if(jawab[i][m] == soal[n][o] && (n!= 0 || count == jawab[i].length()-1)){
+                                                        huruf +=1;
                                                         indexi.push_back(n);
                                                         indexj.push_back(o);
                                                         ans.push_back(soal[n][o]);
@@ -216,11 +223,13 @@ int main(){
                                         }
                                 }
                                 else if(jawab[i][j+1] == soal[k-1][l+1]){
+                                    huruf +=1;
                                     for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k;n>=0;n -= count){
                                             for(int o=l;o<soal[0].length();o++){
                                                 o += count;
                                                 if(jawab[i][m] == soal[n][o]  && (n!= 0 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -253,10 +262,13 @@ int main(){
                                 }
                         }
                         else if(k == 0 || k == 1){
+                            huruf +=1;
                             if((jawab[i][j+1] == soal[k+1][l]) && (jawab[i][j+2] == soal[k+2][l])){ // kebawah
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k+count;n<soal.size();n++){
                                                 if(jawab[i][m] == soal[n][l] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(l);
                                                     ans.push_back(soal[n][l]);
@@ -283,11 +295,13 @@ int main(){
                                         }
                                 }
                             else if(jawab[i][j+1] == soal[k+1][l+1] && jawab[i][j+2] == soal[k+2][l+2]){
+                                    huruf +=1;
                                     for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k+count;n<soal.size();n += count){ // loop bawah soal
                                             for(int o=l;o<soal[0].length();o++){
                                                 o += count; // loop kanan soal
                                                 if(jawab[i][m] == soal[n][o] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -319,11 +333,13 @@ int main(){
                                     }
                                 }
                             else if(jawab[i][j+1] == soal[k+1][l-1] && jawab[i][j+2] == soal[k+2][l-2]){
+                                huruf +=1;
                                 for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k+count;n<soal.size();n += count){
                                             for(int o=l;o>=0;o--){
                                                 o -= count;
                                                 if(jawab[i][m] == soal[n][o] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -356,7 +372,9 @@ int main(){
                             }
                         }
                         else{
+                            huruf +=1;
                             if((jawab[i][j+1] == soal[k-1][l] && jawab[i][j+2] == soal[k-2][l])){ // kebawah
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k-count;n>=0;n--){
                                                 if(jawab[i][m] == soal[n][l] && (n!= 0 || count == jawab[i].length()-1)){
@@ -387,11 +405,13 @@ int main(){
                                         }
                                 }
                                 else if(jawab[i][j+1] == soal[k-1][l-1] && jawab[i][j+2] == soal[k-2][l-2]){ // kiri bawah
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k-count;n>=0;n -= count){ // loop bawah soal
                                                 for(int o=l;o>=0;o--){
                                                     o -= count; // loop kanan soal
                                                     if(jawab[i][m] == soal[n][o] && (n!= 0 || count == jawab[i].length()-1)){
+                                                        huruf +=1;
                                                         indexi.push_back(n);
                                                         indexj.push_back(o);
                                                         ans.push_back(soal[n][o]);
@@ -423,11 +443,13 @@ int main(){
                                         }
                                 }
                                 else if(jawab[i][j+1] == soal[k-1][l+1] && jawab[i][j+2] == soal[k-2][l+2]){
+                                    huruf +=1;
                                     for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k-count;n>=0;n -= count){
                                             for(int o=l;o<soal[0].length();o++){
                                                 o += count;
                                                 if(jawab[i][m] == soal[n][o] && (n!= 0 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -460,9 +482,11 @@ int main(){
                                     
                                 }
                             else if((jawab[i][j+1] == soal[k+1][l] && jawab[i][j+2] == soal[k+2][l])){ // kebawah
+                                        huruf +=1;
                                         for(int m=0;m<jawab[i].length();m++){
                                             for(int n=k+count;n<soal.size();n++){
                                                 if(jawab[i][m] == soal[n][l] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(l);
                                                     ans.push_back(soal[n][l]);
@@ -489,12 +513,14 @@ int main(){
                                         }
                                 }
                             else if(jawab[i][j+1] == soal[k+1][l+1] && jawab[i][j+2] == soal[k+2][l+2]){
+                                    huruf +=1;
                                     for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k;n<soal.size();n++){ // loop bawah soal
                                             n += count;
                                             for(int o=l;o<soal[0].length();o++){
                                                 o += count; // loop kanan soal
                                                 if(jawab[i][m] == soal[n][o] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -531,12 +557,13 @@ int main(){
                                     }
                                 }
                             else if(jawab[i][j+1] == soal[k+1][l-1] && jawab[i][j+2] == soal[k+2][l-2]){
+                                huruf +=1;
                                 for(int m=0;m<jawab[i].length();m++){
                                         for(int n=k+count;n<soal.size();n += count){
                                             for(int o=l;o>=0;o--){
                                                 o -= count;
                                                 if(jawab[i][m] == soal[n][o] && (n!= soal.size()-1 || count == jawab[i].length()-1)){
-                                                    // cout << soal[n][o];
+                                                    huruf +=1;
                                                     indexi.push_back(n);
                                                     indexj.push_back(o);
                                                     ans.push_back(soal[n][o]);
@@ -573,23 +600,14 @@ int main(){
                             }                            
                         }
                     }
+                    else{
+                        huruf +=1;
+                    }
                 }
             }
     }
 
     vector<vector<int>> nums (soal.size(), vector<int>(n));
-    // cout << endl;
-    // for(int i=0;i<soal.size();i++){ // soal kebawah
-    //     for(int j=0;j<soal[0].length();j++){ // soal kesamping
-    //         if(checkIndex(i,j,indicesi,indicesj) == true){
-    //             cout << "\033[1;31m" << soal[i][j] <<"\033[0m" << " ";
-    //         }
-    //         else{
-    //             cout << soal[i][j] << " ";
-    //         }
-    //     }
-    //     cout << endl;
-    // }
     
     for(int i=0;i<soal.size();i++){
         for(int j=0;j<soal[0].length();j++){
@@ -604,8 +622,6 @@ int main(){
         }
         change = (change+1)%6;
     }
-
-    // cout << endl;
 
     for(int i=0;i<soal.size();i++){ // soal kebawah
         for(int j=0;j<soal[0].length();j++){ // soal kesamping
@@ -632,6 +648,11 @@ int main(){
         }
         cout << endl;
     }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "waktu yang dibutuhkan: " << duration.count() << " microseconds" << endl;
+    cout << "jumlah huruf yang dibandingkan: " << huruf << endl;
 
     return 0;
 }
